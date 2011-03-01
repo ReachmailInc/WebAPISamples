158720e0d4f79dfe1df71d57c19387547aa5c6b1
Thomas Solyan <tsolyan@reachmail.com>
2011-03-01 14:19:23 -0800

    edits via git

--- php/PHP_admin-getCurrentUser.php
+++ php/PHP_admin-getCurrentUser.php
@@ -1,5 +1,4 @@
 <?php
-
 //---- API Service URL: AdministrationService\GetCurrentUser
 //---- This service will let you retrieve your API account id. The API account
 //---- is required for accessing other API services.
@@ -17,20 +16,24 @@ $api_service_url = 'https://services.reachmail.net/Rest/Administration/v1/users/
 //---- Intialize cURL and set the options.
 $account_id_request = curl_init();
 $curl_options = array(
-CURLOPT_URL => $api_service_url,
-CURLOPT_HEADER => false,
-CURLOPT_USERPWD => "$account_key\\$username:$password",
-CURLOPT_RETURNTRANSFER => true
-);
+        CURLOPT_URL => $api_service_url,
+        CURLOPT_HEADER => false,
+        CURLOPT_USERPWD => "$account_key\\$username:$password",
+        CURLOPT_RETURNTRANSFER => true
+        );
 curl_setopt_array($account_id_request, $curl_options);
 
 $response = curl_exec($account_id_request);
 
 //---- Load the XML from the response into the simplexml parser and get the
 //---- account id.
+//$xml = simplexml_load_string($response);
+
 $xml = simplexml_load_string($response);
 
 $account_id = $xml->AccountId;
 
 print "\n".$account_id."\n\n";
+//--- save the response into an xml file in the current directory
+echo $account_id->saveXML("write.xml");
 ?>
