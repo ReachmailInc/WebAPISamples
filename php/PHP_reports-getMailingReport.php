299ff6b87eacf1af96c3fb27db3369e880d128cd
Thomas Solyan <tsolyan@reachmail.com>
2011-03-09 08:32:27 -0600

    saves response in an xml file with a lonk toan  xslt file

--- php/PHP_reports-getMailingReport.php
+++ php/PHP_reports-getMailingReport.php
@@ -8,17 +8,17 @@
 //---- administrator or support@reachmail.com. For information on the API
 //---- service URL please refer to the documentation at
 //---- services.reachmail.net
-$account_key = 'account-id';
-$username = 'username';
-$password = 'password';
+$account_key = 'AllinboxC';
+$username = 'Seth';
+$password = '@ll1nb0xRM';
 $api_service_url = 'https://services.reachmail.net/Rest/Reports/v1/mailings/';
 
 //---- For information on getting the API account id please refer to the
 //---- AdministrationService/GetCurrentUser example. For information on getting
 //---- the mailign id please refer to the example for
 //---- ReportService_EnumerateMailingReports 
-$account_id = 'api-account-id';
-$mailing_id = 'mailing-id';
+$account_id = '37c53008-c317-4459-86ee-18febe011e51';
+$mailing_id = 'ece02870-0e48-e011-9a62-001ec9b929f4';
 $api_service_url = $api_service_url.$account_id."/".$mailing_id;
 
 //---- The header variable will be used in the cURL options.
@@ -43,6 +43,15 @@ $mail_report_xml = simplexml_load_string($mail_report_response);
 //---- In this example we've provided a very basic example of accessing XML
 //---- nodes using simplexml objects.
 print_r($mail_report_xml);
-//--- This will save the response as a seperate xml file in the current directory
-echo $mail_report_xml->saveXML("report.xml");
+//--- This will save the response as a seperate xml file, with a link to a xslt style sheet, in the current directory
+$my_file = 'styledReport.xml';
+$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+$data = '<?xml version="1.0"?><?xml-stylesheet type="text/xsl" href="styles.xsl"?>';
+fwrite($handle, $data);
+fclose($handle);
+$my_file = 'styledReport.xml';
+$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+$data = $mail_report_response;
+fwrite($handle, $data);
+fclose($handle);
 ?>
