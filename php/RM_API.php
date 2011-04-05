63bf9cc41dc8a5fef5b3bb51fa1177d1e81819e4
Thomas Solyan <tsolyan@reachmail.com>
2011-04-05 08:27:25 -0700

    PHP lib in progress

--- php/RM_API.php
+++ php/RM_API.php
@@ -17,47 +17,47 @@ Requirements: PHP 5 or higher.
 
 /* Define Static Variables */
   // Administration Service
-    var $base_url = 'https://services.reachmail.net/';
-    var $get_user_url =  'https://services.reachmail.net/Rest/Administration/v1/users/current';
-    var $queue_mailing_url = 'https://services.reachmail.net/Rest/Campaigns/v1/';
+   $base_url = 'https://services.reachmail.net/';
+   $get_user_url =  'https://services.reachmail.net/Rest/Administration/v1/users/current';
+   $queue_mailing_url = 'https://services.reachmail.net/Rest/Campaigns/v1/';
   // Contact Services
-    var $enumerate_fields_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/query/';
-    var $enumerate_lists_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/query/';
-    var $create_list_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/';
-    var $get_list_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/';
-    var $enumerate_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/recipients/query/';
-    var $get_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/recipients';
-    var $create_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/recipients/';
-    var $import_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/import/';
-    var $export_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/export/';
-    var $get_export_status_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/export/status/';
+   $enumerate_fields_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/query/';
+   $enumerate_lists_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/query/';
+   $create_list_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/';
+   $get_list_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/';
+   $enumerate_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/recipients/query/';
+   $get_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/recipients';
+   $create_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/recipients/';
+   $import_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/import/';
+   $export_recipients_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/export/';
+   $get_export_status_url = 'https://services.reachmail.net/Rest/Contacts/v1/lists/export/status/';
   //Mailing Services
-    var $enumerate_mailings_url = 'https://services.reachmail.net/Rest/Content/Mailings/v1/query/';
-    var $get_mailing_url = 'https://services.reachmail.net//Rest/Content/Mailings/v1/';
-    var $delete_mailing_url = 'https://services.reachmail.net//Rest/Content/Mailings/v1/';
-    var $create_mailing_url = 'https://services.reachmail.net/REST/Content/Mailings/v1/';
+   $enumerate_mailings_url = 'https://services.reachmail.net/Rest/Content/Mailings/v1/query/';
+   $get_mailing_url = 'https://services.reachmail.net//Rest/Content/Mailings/v1/';
+   $delete_mailing_url = 'https://services.reachmail.net//Rest/Content/Mailings/v1/';
+   $create_mailing_url = 'https://services.reachmail.net/REST/Content/Mailings/v1/';
   //Report Services
-    var $enumerate_mailing_reports_url = 'https://services.reachmail.net/Rest/Reports/v1/mailings/query/';
-    var $get_mailing_report_url = 'https://services.reachmail.net/Rest/Reports/v1/mailings/';
-    var $get_mailing_report_summary_url = 'https://services.reachmail.net/Rest/Reporting/Content/Mailings/v1/Summary/';
+   $enumerate_mailing_reports_url = 'https://services.reachmail.net/Rest/Reports/v1/mailings/query/';
+   $get_mailing_report_url = 'https://services.reachmail.net/Rest/Reports/v1/mailings/';
+   $get_mailing_report_summary_url = 'https://services.reachmail.net/Rest/Reporting/Content/Mailings/v1/Summary/';
   //Data Services
-    var $data_upload_url = 'https://services.reachmail.net/Rest/Data/';
-    var $data_exist_url = 'https://services.reachmail.net/Rest/Data/exists/';
+   $data_upload_url = 'https://services.reachmail.net/Rest/Data/';
+   $data_exist_url = 'https://services.reachmail.net/Rest/Data/exists/';
 	
 	class login{
-	            private $account_key;
-		    private $username;
-		    private $password;
+	       private  $account_key;
+		   private  $username;
+		   private  $password;
 		
-		function _construct($account_key = NULL, $username = NULL, $password = NULL) {
+		function __CONSTRUCT($account_key = NULL, $username = NULL, $password = NULL) {
 					$this->account_key = $account_key;
 					$this->username = $username;
 					$this->password = $password;		
 		}	
 		
-
 		function getUser() {
-		    $account_id_request = curl_init();
+					global $get_user_url;
+		            $account_id_request = curl_init();
                     $curl_options = array(
                         CURLOPT_URL => $get_user_url,
                         CURLOPT_HEADER => false,
@@ -66,8 +66,9 @@ Requirements: PHP 5 or higher.
                     );
                     curl_setopt_array($account_id_request, $curl_options);
                     $response = curl_exec($account_id_request);
-		    $xml = simplexml_load_string($response);
-                    $account_id = $xml->AccountId;                   
+		            $xml = simplexml_load_string($response);
+                    $account_id = $xml->AccountId;   
+					print "\n".$account_id."\n\n";					
                     echo $account_id->saveXML("user.xml");
 		}
 }
