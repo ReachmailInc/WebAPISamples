e528dea543417482d7e692a753bffcdddf612acf
Thomas Solyan <tsolyan@reachmail.com>
2011-04-13 16:21:55 -0500

    php_lib inProgress

--- php/RM_API.php
+++ php/RM_API.php
@@ -63,7 +63,7 @@ Requirements: PHP 5 or higher.
  * $queueMail->rm_queueMail($account_id, $request_body);
  *
  * @param string $account_id  The account_id returned from the Get User service.
- * @param string $reuest_body In xml and containg parameters delineated here https://services.reachmail.net/sdk/.
+ * @param string $request_body In xml and containg parameters delineated here https://services.reachmail.net/sdk/.
  * 
  * @return string The queue_id in both the standard output and as queueId.xml.
 */		
@@ -135,7 +135,7 @@ Requirements: PHP 5 or higher.
  * $enumerateLists->rm_enumerateLists($account_id, $request_body);
  *
  * @param string $account_id The account_id returned from the Get User service.
- * @param string $reuest_body In xml and containg parameters delineated here https://services.reachmail.net/sdk/.
+ * @param string $request_body In xml and containg parameters delineated here https://services.reachmail.net/sdk/.
  *
  * @return string A list of list-is's and other requested data in both standard output and as lists.xml.
 */		
@@ -178,7 +178,7 @@ Requirements: PHP 5 or higher.
  * $createList->rm_createList($account_id, $request_body);
  * 
  * @param string $account_id The account_id returned from the Get User service.
- * @param string $reuest_body In xml and containg parameters delineated here https://services.reachmail.net/sdk/.
+ * @param string $request_body In xml and containg parameters delineated here https://services.reachmail.net/sdk/.
  *
  * @return string The new list_id in both the standard output and as listId.xml.
 */		
