72060ceda7e33cc5a3aff1771dc42ed72dd696ca
Thomas Solyan <tsolyan@reachmail.com>
2011-04-14 14:33:22 -0500

    php_lib inProgress

--- php/RM_API.php
+++ php/RM_API.php
@@ -274,6 +274,31 @@ Requirements: PHP 5 or higher.
 					}
 		}	
 /**
+ * Create and Populate: creates and populates a list in your ReachMail account.
+ *
+ * Both the $request_body xml strings need to be set up as per your lists specific needs.
+ * $createAndPopulate = RM_API('ACME','admin','1234ABC');
+ * $createAndPopulate->rm_createAndPopulate($account_id, $file);
+ *
+ * @param string $account_id  The account_id returned from the Get User service.
+ * @param string $file Must be a path to a comma seperated list with open permissions.
+ *
+ * @return string Sets up a populated list in the account and returnd an importID.
+*/
+		
+	function rm_createAndPopulate($account_id, $file) {
+				$request_body = "<ListProperties><Fields><Field>Email</Field><Field>FullName</Field></Fields><Name>API-TestList</Name></ListProperties>";
+				$createList = new RM_API($this->_account_key, $this->_username, $this->_password);
+				$createList->rm_createList($account_id, $request_body);
+				$list_id = simplexml_load_string(file_get_contents('listId.xml'));								
+				$uploadFile = new RM_API($this->_account_key, $this->_username, $this->_password);
+				$uploadFile->rm_uploadData($file);
+				$data_id = simplexml_load_string(file_get_contents('uploadId.xml'));				
+				$request_body = "<Parameters><DataId>$data_id</DataId><FieldMappings><FieldMapping><DestinationFieldName>Email</DestinationFieldName><SourceFieldPosition>1</SourceFieldPosition></FieldMapping><FieldMapping><DestinationFieldName>FullName</DestinationFieldName><SourceFieldPosition>2</SourceFieldPosition></FieldMapping></FieldMappings><ImportOptions><CharacterSeperatedOptions><Delimiter>Comma</Delimiter></CharacterSeperatedOptions><Format>CharacterSeperated</Format></ImportOptions></Parameters>";
+				$importRecipients = new RM_API($this->_account_key, $this->_username, $this->_password);
+				$importRecipients->rm_importRecipients($account_id, $list_id, $request_body);
+}		
+/**
  * Enumerate Recipients returns all records in a list that meet the request parameters.
  * 
  * $enumerateRecipients = new RM_API('ACME','admin','1234ABC');
