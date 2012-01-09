1150e236fd811b22bbcc237f2aa5293858d07607
danielsen <dnielsen@sdf.lonestar.org>
2012-01-09 18:47:05 +0000

    Update enumerate_mailing_reports to correct report service

--- perl/enumerate_mailings_report.pl
+++ perl/enumerate_mailings_report.pl
@@ -11,11 +11,11 @@ use HTTP::Request::Common qw(POST);
 #---- the account_id and list_id variables need to be retrieved using API
 #---- services, please refer to services
 #---- AdministrationService\GetCurrentUser and EnumerateLists respectively
-$account_key = 'account-id';
+$account_key = 'account_id';
 $username = 'username';
 $password = 'password';
-$api_service_url = 'https://services.reachmail.net/Rest/Reporting/Content/Mailings/v1/query/';
-$account_id = 'api-acount-id';
+$api_service_url = 'https://services.reachmail.net/Rest/Reports/v1/mailings/query/';
+$account_id = 'api_account_id';
 
 #---- set the user agent and content type ----
 $ua = LWP::UserAgent->new;
@@ -24,7 +24,7 @@ $ua->default_header('Content-Type' => "application/xml");
 
 #---- set URL and authentication for lists, make the request ----
 $report_url = HTTP::Request->new(POST => "$api_service_url$account_id");
-$report_url->content('<MailingFilter><DeliveryScheduledAfter>2010-01-01</DeliveryScheduledAfter><DeliveryScheduledBefore>2010-09-20</DeliveryScheduledBefore></MailingFilter>');
+$report_url->content('<MailingReportFilter><ScheduledDeliveryOnOrAfter>2011-10-01</ScheduledDeliveryOnOrAfter></MailingReportFilter>');
 $report_url->authorization_basic("$account_key\\$username", "$password");
 $report_request = $ua->request($report_url);
 
@@ -33,9 +33,9 @@ $report_xml = $report_request->content;
 $parser = XML::LibXML->new();
 $report_dom = $parser->parse_string($report_xml);
 
-$mailings = $report_dom->findnodes('Mailings');
+$mailings = $report_dom->findnodes('MailingReports');
 foreach $mailing ($mailings->get_nodelist){
-	@mail_id = $mailing->findnodes('Mailing/Id');
+	@mail_id = $mailing->findnodes('MailingReport/MailingId');
 }
 
 $mail_count = @mail_id;
