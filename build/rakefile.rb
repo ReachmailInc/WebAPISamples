2b532ee28047de195c8d2c783d0e00134e3aebb4
mob <mob@mikeobrien.net>
2012-10-22 15:57:35 -0400

    Build script

--- build/rakefile.rb
+++ build/rakefile.rb
@@ -1,8 +1,18 @@
-reportsPath = "reports"
-version = ENV["BUILD_NUMBER"]
+require 'net/http'
+
+reportsPath = 'reports'
+version = ENV['BUILD_NUMBER']
 
 task :build => [:downloadApiSpec]#[:buildDotNet]
 
 task :downloadApiSpec do
-  puts 'oh hai'
+  puts 'Downloading the API spec...'
+  Net::HTTP.start('services.reachmail.net') do |http|
+      resp = http.get('documentation/data')
+      resp['accept'] = 'application/json'
+      open('../apispec.json', 'wb') do |file|
+          file.write(resp.body)
+      end
+  end
+  puts 'Download complete.'
 end
