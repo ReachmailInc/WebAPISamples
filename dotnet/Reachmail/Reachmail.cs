e1ffe6be1b09f9895c4a73abe77bcd7c9aa42165
mob <mob@mikeobrien.net>
2012-10-22 21:06:08 -0400

    Some updates

--- dotnet/Reachmail/Reachmail.cs
+++ dotnet/Reachmail/Reachmail.cs
@@ -8,23 +8,5 @@ namespace Reachmail
 {
     public class Reachmail
     {
-        public Reachmail() 
-        {
-            Administration = new Administration();
-            Campaigns = new Campaigns();
-            Contacts = new Contacts();
-            Content Library = new Content Library();
-            Data = new Data();
-            Mailings = new Mailings();
-            Reports = new Reports();
-        }
-        
-        public Administration Administration { get; private set; }
-        public Campaigns Campaigns { get; private set; }
-        public Contacts Contacts { get; private set; }
-        public Content Library Content Library { get; private set; }
-        public Data Data { get; private set; }
-        public Mailings Mailings { get; private set; }
-        public Reports Reports { get; private set; }
     }    
 }
