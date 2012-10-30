11757fa72f31d8b7a90776d252f55e9d50834dc6
mob <mob@mikeobrien.net>
2012-10-29 21:19:01 -0400

    API wrapper template updates.

--- dotnet/Reachmail/Reachmail.Template.cs
+++ dotnet/Reachmail/Reachmail.Template.cs
@@ -1,22 +1,76 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Reachmail
+namespace ReachmailApi
 {
     public class Reachmail
     {
-        public Reachmail() 
+        public Reachmail(string accountKey, string username, string password) 
         {
-{{#Modules}}
-            {{Name}} = new {{Name}}();
-{{/Modules}}        
+            var httpClient = new HttpClient(accountKey + @"\" + username, password);
+    {{#rootModules}}
+        {{.}} = new {{.}}.Module(httpClient);
+    {{/rootModules}}
+    
+            httpClient.AddParameterDefault("accountId", Administration.Users.Current.Get().AccountId);
         }
-        
-{{#Modules}}
-        public {{Name}} {{Name}} { get; private set; }
-{{/Modules}}
-    }    
+
+    {{#rootModules}}
+    public {{.}}.Module {{.}} { get; set; }
+    {{/rootModules}}
+}   
+
+    {{#modules}}
+namespace {{namespace}}
+    {
+        public class Module
+        {
+            private readonly HttpClient _httpClient;
+
+            internal Module(HttpClient httpClient) 
+            {
+                _httpClient = httpClient;
+    {{#modules}}
+            {{.}} = new {{.}}.Module(httpClient);
+    {{/modules}}
+        }
+
+    {{#modules}}
+        public {{.}}.Module {{.}} { get; set; }
+    {{/modules}}
+    {{#endpoints}}
+
+            /// <summary>{{Comments}}</summary> 
+            {{#UrlParameters}}/// <param name="{{Name}}">{{Comments}}</param>
+            {{/UrlParameters}}
+{{#Request}}/// <param name="request">{{Comments}}</param>
+{{/Request}}
+            {{#QuerystringParameters}}/// <param name="{{Name}}">{{Comments}}</param>
+            {{/QuerystringParameters}}
+public {{returnType}} {{Method}}({{parameters}}) 
+            {
+                {{returnKeyword}}_httpClient.Execute("{{Url}}", 
+                    HttpClient.Verb.{{Method}}, 
+                    new Dictionary<string, object> { {{{urlArguments}}} }, 
+                    new Dictionary<string, object> { {{{querystringArguments}}} });
+            }
+        {{/endpoints}}
+}
+    }   
+    {{#endpoints}}
+    
+    namespace {{namespace}}.{{Method}}
+    {
+        public class Request 
+        {
+        }
+
+        public class Response 
+        {
+        }
+    }
+        {{/endpoints}}
+
+    {{/modules}}
+
 }
