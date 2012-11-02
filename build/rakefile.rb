4691b0ab4907c586f484e57acbc5c7edb4d6aff2
mob <mob@mikeobrien.net>
2012-11-02 17:28:40 -0400

    Added a CI build for the web api samples.

--- build/rakefile.rb
+++ build/rakefile.rb
@@ -5,8 +5,8 @@ task :build => [:downloadApiSpec, :buildDotNet]
 task :deploy => [:downloadApiSpec, :deployDotNet]
 
 task :downloadApiSpec do
-  puts 'Downloading the API spec...'
-  Net::HTTP.start(ENV['SPEC_HOST']) do |http|
+  puts "Downloading the API spec from #{ENV['API_HOST']}..."
+  Net::HTTP.start(ENV['API_HOST']) do |http|
       resp = http.get('/documentation/data', { 'accept' => 'application/json'})
       open('apispec.json', 'wb') do |file|
           file.write(resp.body)
