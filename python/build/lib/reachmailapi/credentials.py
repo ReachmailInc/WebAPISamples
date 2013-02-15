bcd7472de76112bed12633cce1df19882b4f4d3d
Dan Nielsen <dnielsen@helios.(none)>
2013-02-15 17:13:22 -0600

    Removed build files

--- python/build/lib/reachmailapi/credentials.py
+++
@@ -1,8 +0,0 @@
-# Reachmail API credentials class
-
-class Login(object):
-	def __init__(self, account_key, username, password):
-		self.acct_key = account_key.upper()
-		self.username = username
-		self.password = password
-		self.api_user = self.acct_key + '\\' + username
