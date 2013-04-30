e9cab1bc8c51d27f0d7388ee37a0b574ae7bdf9a
mob <mob@mikeobrien.net>
2013-04-30 13:19:58 -0400

    Rolled back a few .NET wrapper changes.

--- build/csharp-wrapper-task.rb
+++ build/csharp-wrapper-task.rb
@@ -61,7 +61,7 @@ class CSharpWrapper
 			if type.start_with?('ArrayOf') 
 				"List<#{get_data_type(type.gsub(/ArrayOf/, ''))}>"
 			else
-				raise "Invalid type #{if required then 'required' else 'optional' end} #{type}."
+				type + (required ? '' : '?')
 			end
         end
     end
