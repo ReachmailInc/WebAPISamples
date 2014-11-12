d3a163457e87866b769cd625afd4fce9ed8e5996
mob <mob@mikeobrien.net>
2014-11-12 17:59:35 -0500

    .NET wrapper build WIP.

--- dotnet/wrapper.js
+++ dotnet/wrapper.js
@@ -242,7 +242,7 @@ Handlebars.registerHelper('simpleClrType', function (name, optional, isEnum) {
 // Module
 
 module.exports = function(jsonPath, templatePath, outputPath) {
-    var json = JSON.parse(fs.readFileSync(json, { encoding: 'UTF8' }));
-    var template = Handlebars.compile(template, { encoding: 'UTF8' });
+    var json = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'UTF8' }));
+    var template = Handlebars.compile(templatePath, { encoding: 'UTF8' });
     fs.writeFileSync(outputPath, tempate(json));
 }
