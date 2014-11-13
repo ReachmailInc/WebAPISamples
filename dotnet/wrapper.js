e96734a25439dc15349259079dcc7897680e716f
mob <mob@mikeobrien.net>
2014-11-12 22:06:38 -0500

    .NET wrapper build WIP.

--- dotnet/wrapper.js
+++ dotnet/wrapper.js
@@ -181,8 +181,8 @@ Handlebars.registerHelper('hasRequest', function (options) {
 });
 
 Handlebars.registerHelper('hasResponse', function (options) {
-    if (supportsStream(this.Response)) return options.fn(_.extend(this, { stream: true }));
-    if (this.Response.Body.Type) return options.fn(_.extend(this, { stream: false }));
+    if (supportsStream(this.Response)) return options.fn(_.extend({ stream: true }, this));
+    if (this.Response.Body.Type) return options.fn(_.extend({ stream: false }, this));
     return options.inverse();
 });
 
