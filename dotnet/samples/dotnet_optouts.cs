0ad279b8b6ff2cee8b3d4ea3e97dee5f4949d4d4
mob <mob@mikeobrien.net>
2014-12-23 03:04:33 -0500

    Easy SMTP sample files.

--- dotnet/samples/dotnet_optouts.cs
+++ dotnet/samples/dotnet_optouts.cs
@@ -1,1 +1,10 @@
-under construction.
+using System;
+
+public void Main() 
+{
+    var reachmail = Reachmail.Api.Connect("<API Token>");
+
+    var enddate = new DateTime(1985, 10, 26, 1, 20, 0);
+    var startdate = new DateTime(1985, 10, 26, 1, 20, 0);
+    var result = reachmail.Reports.Easysmtp.Optouts.Get(enddate, startdate);
+}
--- dotnet/samples/dotnet_send.cs
+++ dotnet/samples/dotnet_send.cs
@@ -1,1 +1,36 @@
-under construction.
+using System;
+using Reachmail.Easysmtp.Post.Request;
+
+public void Main() 
+{
+    var reachmail = Reachmail.Api.Connect("<API Token>");
+
+    var request = new DeliveryRequest { 
+        FromAddress = "from@from.com",
+        Recipients = new Recipients { 
+            new Recipient { 
+                Address = "to@to.com"
+            }
+        },
+        Subject = "Subject",
+        BodyText = "Text",
+        BodyHtml = "<p>html</p>",
+        Headers = new Headers { 
+            { "name", "value" }
+        },
+        Attachments = new Attachments { 
+            new EmailAttachment { 
+                Filename = "text.txt",
+                Data = "b2ggaGFp", // Base64 encoded
+                ContentType = "text/plain",
+                ContentDisposition = "attachment",
+                Cid = "<text.txt>"
+            }
+        },
+        Tracking = true,
+        FooterAddress = "footer@footer.com",
+        SignatureDomain = "signature.net"
+    };
+
+    var result = reachmail.Easysmtp.Post(request);
+}
